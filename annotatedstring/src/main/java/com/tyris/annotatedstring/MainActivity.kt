74c8cab251272e21420121268c03147b1d02df50
Ivan Pons <ivan.pons@tyris-software.com>
2024-11-12 08:45:02 +0100

    feat(app): add AnnotatedString

--- annotatedstring/src/main/java/com/tyris/annotatedstring/MainActivity.kt
+++ annotatedstring/src/main/java/com/tyris/annotatedstring/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.tyris.annotatedstring
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import com.example.annotatedstring.R
 import com.tyris.annotatedstring.ui.theme.ComposablesTyrisTheme
+import com.tyris.annotatedstring.ui.theme.Poppins
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposablesTyrisTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AnnotatedView(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +44,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposablesTyrisTheme {
-        Greeting("Android")
+fun AnnotatedView(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(vertical = 32.dp)
+            .padding(top = 16.dp)
+    ) {
+        val annotatedString = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontFamily = Poppins,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                append(stringResource(id = R.string.dont_have_an_account) + " ")
+                pushStringAnnotation(
+                    tag = "clickable_text",
+                    annotation = stringResource(id = R.string.sign_up)
+                )
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontFamily = Poppins
+                    )
+                ) {
+                    append(stringResource(id = R.string.sign_up))
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentAlignment = Alignment.BottomCenter
+        ){
+            ClickableText(
+                text = annotatedString,
+                onClick = { offset ->
+                    annotatedString.getStringAnnotations(
+                        tag = "clickable_text",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
+                        Log.d("AnnotatedString", "Clicked on: ${it.item}")
+                    }
+                }
+            )
+        }
     }
 }
--- annotatedstring/src/main/java/com/tyris/annotatedstring/ui/theme/Type.kt
+++ annotatedstring/src/main/java/com/tyris/annotatedstring/ui/theme/Type.kt
@@ -2,10 +2,35 @@ package com.tyris.annotatedstring.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.annotatedstring.R
 
+
+val Poppins = FontFamily(
+    Font(
+        resId = R.font.poppins_light,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resId = R.font.poppins_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.poppins_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.poppins_semibold,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resId = R.font.poppins_bold,
+        weight = FontWeight.Bold
+    ),
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
